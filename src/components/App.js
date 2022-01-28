c6987a1a6afac84d0daec95020957438c9535805
GabrielaDana <gabriela.dana.pacheco@gmail.com>
2022-01-28 16:11:16 -0300

    28 enero

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+import pokemon from '../data/pokemon/pokemon.js';
+console.log(pokemon);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -14,16 +14,21 @@
 //   .catch(console.error);
 //
 const App = () => {
-  const card = document.createElement("div");
-  card.className = "card";
+  //
+  let poke = pokemon.items;
+  //Primer div creado, padre de los siguientes, necesita return para aparecer dentro de root
+  const board = document.createElement("div");
+  board.className = "board";
 
-  const cardInner = document.createElement("div");
-  cardInner.className = "cardInner";
-  card.appendChild(cardInner);
-
+  poke.forEach(function(index, element){
+    const card = document.createElement("div");
+    card.className = "card";
+    board.appendChild(card);
+    console.log("entramos a div card");
+ 
   const cardFront = document.createElement("div");
   cardFront.className = "cardFront";
-  cardInner.appendChild(cardFront);
+  card.appendChild(cardFront);
 
   const cardImage = document.createElement("img");
   cardImage.alt = "pokeball";
@@ -33,8 +38,18 @@ const App = () => {
 
   const cardBack = document.createElement("div");
   cardBack.className = "cardBack";
-  cardBack.textContent = "Atrás";
-  cardInner.appendChild(cardBack);
-  return card;
+  card.appendChild(cardBack);
+  
+  poke.forEach(function(element, index){
+    const backImage = document.createElement("img");
+    backImage.src = poke[index].image;
+    cardBack.appendChild(backImage);
+    // console.log(poke.image);
+  });
+
+});
+ 
+  //Return para que muestre el div como padre y aparezca en el html
+  return board;
 };
 export default App;
--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@
 import App from './components/App.js';
 
 document.getElementById("root").appendChild(App());
+
+
+function flip(){
+document.querySelector(".card").className += "Flip";
+};
+document.querySelector(".image").addEventListener("click", flip);
