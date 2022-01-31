b53b7d2a277fa1fa28ca7c8b2585f65ce1f1dbed
GabrielaDana <gabriela.dana.pacheco@gmail.com>
2022-01-31 12:21:47 -0300

    cambios office hour

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import App from './components/App.js';
 document.getElementById("root").appendChild(App());
 
 
-function flip(){
+// function flip(){
+//    const carta = document.querySelectorAll(".card");
+//    console.log(carta);
+// carta.className += "Flip";
+// };
 
-document.querySelector(".card").className += "Flip";
-};
-
-document.querySelector(".image").addEventListener("click", flip);
+const imagen = document.querySelectorAll(".card");
+console.log(imagen);
+for(let i = 0; i < imagen.length; i++){
+    imagen[i].addEventListener("click", function(event){
+        // console.log(event.target);
+        event.target.parentElement.parentElement.classList.add("flip")
+    });
+}
