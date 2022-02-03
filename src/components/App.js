79d4193b241ca6f99397606f6d09000775bd2356
GabrielaDana <gabriela.dana.pacheco@gmail.com>
2022-02-03 13:10:14 -0300

    orden

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,19 @@ import pokemon from '../data/pokemon/pokemon.js';
 
 const App = () => {
   const poke = pokemon.items;
-    var doublepoke = poke.concat(poke);
+  var doublepoke = poke.concat(poke);
 
-    function shuffle(){
+  function shuffle() {
 
-      doublepoke.sort(() => Math.random() - 0.5);
-    }
-      shuffle();
-    
-//Primer div creado, padre de los siguientes, necesita return para aparecer dentro de root
-    const board = document.createElement("div");
-    board.className = "board";
+    doublepoke.sort(() => Math.random() - 0.5);
+  }
+  shuffle();
 
-  for(let i = 0; i<doublepoke.length; i++){
+  //Primer div creado, padre de los siguientes, necesita return para aparecer dentro de root
+  const board = document.createElement("div");
+  board.className = "board";
+
+  for (let i = 0; i < doublepoke.length; i++) {
 
 
     const card = document.createElement("div");
@@ -41,14 +41,14 @@ const App = () => {
 
 
     const cardImage = document.createElement("img");
-    cardImage.alt = "pokeball";   
+    cardImage.alt = "pokeball";
     cardImage.className = "image";
     cardImage.src = "images/pokeball.png";
     cardFront.appendChild(cardImage);
 
-}
+  }
 
-//Return para que muestre el div como padre y aparezca en el html
+  //Return para que muestre el div como padre y aparezca en el html
   return board;
 };
 
--- src/components/game.js
+++ src/components/game.js
@@ -2,29 +2,29 @@
 const flip = document.getElementsByClassName("flip");
 let contador = 0;
 
-function checkCard(){
+function checkCard() {
     let pkmnFlip = document.getElementsByClassName("pkmnFlip");
-    
-        if (pkmnFlip.length === 2){
-        
-        if (pkmnFlip[0].getAttribute("id") === pkmnFlip[1].getAttribute("id")){
+
+    if (pkmnFlip.length === 2) {
+
+        if (pkmnFlip[0].getAttribute("id") === pkmnFlip[1].getAttribute("id")) {
             ++contador;
 
             match();
-        }   
+        }
 
         else {
             setTimeout(() =>
-            flip[1].classList.remove("flip"),1000);
+                flip[1].classList.remove("flip"), 1000);
             setTimeout(() =>
-            flip[0].classList.remove("flip"),1000);
-    
-            pkmnFlip[1].classList.remove("pkmnFlip");
-            pkmnFlip[0].classList.remove("pkmnFlip");   
-        }
-        if(contador === 9){
+                flip[0].classList.remove("flip"), 1000);
 
-            setTimeout(()=> 
+            pkmnFlip[1].classList.remove("pkmnFlip");
+            pkmnFlip[0].classList.remove("pkmnFlip");
+        }
+        if (contador === 9) {
+
+            setTimeout(() =>
             (Swal.fire({
                 title: '¡FELICITACIONES!',
                 text: 'Eres un verdadero maestro Pokemon',
@@ -32,22 +32,22 @@ function checkCard(){
                 imageWidth: 400,
                 imageHeight: 250,
             }
-            )),850);
-            
+            )), 850);
+
         }
     }
-    
-    }
-    
-    function match() {
-        const pkmnFlip = document.getElementsByClassName("pkmnFlip");
-        flip[1].classList.add("match"); 
-        flip[0].classList.add("match"); 
-        flip[1].classList.remove("flip");
-        flip[0].classList.remove("flip");
-    
-        pkmnFlip[1].classList.remove("pkmnFlip");
-        pkmnFlip[0].classList.remove("pkmnFlip");
-    }
 
-    export {checkCard, match};
+}
+
+function match() {
+    const pkmnFlip = document.getElementsByClassName("pkmnFlip");
+    flip[1].classList.add("match");
+    flip[0].classList.add("match");
+    flip[1].classList.remove("flip");
+    flip[0].classList.remove("flip");
+
+    pkmnFlip[1].classList.remove("pkmnFlip");
+    pkmnFlip[0].classList.remove("pkmnFlip");
+}
+
+export { checkCard, match };
